fade26afbfc34742b0ce6a712e9620c1ba836f18
Om Bharsakle <ombharsakle418@gmail.com>
2024-09-02 17:41:14 +0530

    28/08/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
-import 'Screens/MakeYourTheme/View/MakeTheme.dart';
-import 'Screens/ShareSavePage/View/ShareSaveScreen.dart';
+
+import 'Screens/HomeScreen/View/Home_Page.dart';
 import 'Screens/SplashScreen/SplashScreen.dart';
 
-void main()
-{
-  runApp(const QuotesApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
-class QuotesApp extends StatelessWidget {
-  const QuotesApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
-      getPages: [
-        GetPage(name: '/', page: () =>  const SplashScreen()),
-        GetPage(name: '/MakeTheme', page: () =>  const MakeTheme()),
-        GetPage(name: '/Category', page: () =>  const ShareSaveScreen()),
-      ],
+      home: const SplashScreen(),
     );
   }
 }
